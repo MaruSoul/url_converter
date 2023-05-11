b39729389b0b97eada756a40a365e224bcdb1ee4
MaruSoul <soulmeri0@gmail.com>
2023-05-11 16:12:39 +0300

    added database

---
+++ src/Models/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tmolbik\PhpPro\Models; 
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Database 
+{
+    public function __construct() 
+    {
+        $capsule = new Capsule;
+        $capsule->addConnection([
+             'driver' => 'mysql',
+             'host' => 'db',
+             'database' => 'database',
+             'username' => 'user',
+             'password' => 'root',
+             'charset' => 'utf8',
+             'collation' => 'utf8_unicode_ci',
+             'prefix' => '',
+        ]);
+
+        $capsule->bootEloquent();
+        $capsule->setAsGlobal();
+    }
+}
